6d25c161c44b0ead3445a64bdf8ebf51b29ae795
Alex Beltyukov <beltyukov.1@gmail.com>
2015-02-22 20:00:54 -0600

    Add stack words

--- playground.js
+++ playground.js
@@ -5,12 +5,17 @@ var terp = new Scratch();
 
 // Check print words
 terp.addWords(words.PrintingWords);
-console.log('---PRINT---');
-terp.run("10 pstack");
-terp.run("1 2 3 print print print");
+//console.log('---PRINT---');
+//terp.run("10 pstack");
+//terp.run("1 2 3 print print print");
 
 // Check math words
-terp.addWords(words.MathWords);
-console.log('---MATH---');
-terp.run('2 2 + print');
-terp.run('3 3 * 4 4 * + sqrt print');
+//terp.addWords(words.MathWords);
+//console.log('---MATH---');
+//terp.run('2 2 + print');
+//terp.run('3 3 * 4 4 * + sqrt print');
+
+// Check stack words
+terp.addWords(words.StackWords);
+console.log('---STACK---');
+terp.run('1 2 3 rot pstack');
--- words.js
+++ words.js
@@ -1,49 +1,110 @@
 module.exports = {
     // Recognize print-related words
     PrintingWords: {
-        // Log and discard top of stack
+        // Log and discard top of stack (TOS)
         "PRINT": function(terp) {
             if (terp.stack.length < 1) {
                 throw 'Not enough items on stack';
             }
             var tos = terp.stack.pop();
-            console.log("top of stack", tos);
+            console.log("Top of stack:", tos);
         },
-        // Log out contents of stack
+        // Log contents of stack
         "PSTACK": function(terp) {
-            console.log("terp stack", terp.stack)
+            console.log("Stack:", terp.stack, "<- TOS")
         }
     },
     // Recognize certain math operations
     MathWords: {
-        "+": function (terp) {
-            if (terp.stack.length < 2) throw "Not enough items on stack";
+        "+": function(terp) {
+            if (terp.stack.length < 2) {
+                throw "Not enough items on stack";
+            }
             var tos = terp.stack.pop();
             var _2os = terp.stack.pop();
             terp.stack.push(_2os + tos);
         },
-        "-": function (terp) {
-            if (terp.stack.length < 2) throw "Not enough items on stack";
+        "-": function(terp) {
+            if (terp.stack.length < 2) {
+                throw "Not enough items on stack";
+            }
             var tos = terp.stack.pop();
             var _2os = terp.stack.pop();
             terp.stack.push(_2os - tos);
         },
-        "*": function (terp) {
-            if (terp.stack.length < 2) throw "Not enough items on stack";
+        "*": function(terp) {
+            if (terp.stack.length < 2) {
+                throw "Not enough items on stack";
+            }
             var tos = terp.stack.pop();
             var _2os = terp.stack.pop();
             terp.stack.push(_2os * tos);
         },
-        "/": function (terp) {
-            if (terp.stack.length < 2) throw "Not enough items on stack";
+        "/": function(terp) {
+            if (terp.stack.length < 2) {
+                throw "Not enough items on stack";
+            }
             var tos = terp.stack.pop();
             var _2os = terp.stack.pop();
             terp.stack.push(_2os / tos);
         },
-        "SQRT": function (terp) {
-            if (terp.stack.length < 1) throw "Not enough items on stack";
+        "SQRT": function(terp) {
+            if (terp.stack.length < 2) {
+                throw "Not enough items on stack";
+            }
             var tos = terp.stack.pop();
             terp.stack.push(Math.sqrt(tos));
+        }
+    },
+    StackWords: {
+        //Duplicate TOS
+        "DUP": function(terp) {
+            if (terp.stack.length < 1) {
+                throw "Not enough items on stack";
+            }
+            var tos = terp.stack.pop();
+            terp.stack.push(tos);
+            terp.stack.push(tos);
+        },
+        // Throw away TOS
+        "DROP": function(terp) {
+            if (terp.stack.length < 1) {
+                throw "Not enough items on stack";
+            }
+            terp.stack.pop();
+        },
+        // Exchange positions of TOS and second item on stack (2OS)
+        "SWAP": function(terp) {
+            if (terp.stack.length < 2) {
+                throw "Not enough items on stack";
+            }
+            var tos = terp.stack.pop();
+            var _2os = terp.stack.pop();
+            terp.stack.push(tos);
+            terp.stack.push(_2os);
+        },
+        // Copy 2OS on TOS
+        "OVER": function(terp) {
+            if (terp.stack.length < 2) {
+                throw "Not enough items on stack";
+            }
+            var tos = terp.stack.pop();
+            var _2os = terp.stack.pop();
+            terp.stack.push(_2os);
+            terp.stack.push(tos);
+            terp.stack.push(_2os);
+        },
+        // Bring 3rd item on stack (3OS) to top
+        "ROT": function(terp) {
+            if (terp.stack.length < 3) {
+                throw "Not enough items on stack";
+            }
+            var tos = terp.stack.pop();
+            var _2os = terp.stack.pop();
+            var _3os = terp.stack.pop();
+            terp.stack.push(_2os);
+            terp.stack.push(tos);
+            terp.stack.push(_3os);
         }
     }
 };
